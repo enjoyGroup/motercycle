12969ca25a2803a873ea8d067b3384f928a2471a
enjoyGroup <ponpon.osk@gmail.com>
2015-01-26 23:46:20 +0700

    Commit By Pound fix expirydate 26/01/2015

--- motorcyclesWeb/src/th/go/motorcycles/web/enjoy/servlet/LoginServlet.java
+++ motorcyclesWeb/src/th/go/motorcycles/web/enjoy/servlet/LoginServlet.java
@@ -1,6 +1,10 @@
 package th.go.motorcycles.web.enjoy.servlet;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +13,7 @@ import javax.servlet.http.HttpSession;
 
 import th.go.motorcycles.app.enjoy.bean.UserDetailsBean;
 import th.go.motorcycles.app.enjoy.dao.UserDetailsDao;
-import th.go.motorcycles.app.enjoy.main.ConfigFile;
+import th.go.motorcycles.app.enjoy.exception.EnjoyException;
 import th.go.motorcycles.web.enjoy.common.EnjoyStandardSvc;
 import th.go.motorcycles.web.enjoy.logger.LogWrapper;
 import th.go.motorcycles.web.enjoy.utils.MotorUtil;
@@ -37,6 +41,8 @@ import th.go.motorcycles.web.enjoy.utils.MotorUtil;
         	passWord 	= MotorUtil.nullToStr(request.getParameter("passWord"));
         	userDao		= new UserDetailsDao();
         	
+        	this.checkExpiryDate();
+        	
         	logger.info("[EnjoyLoginSvc][execute] userId 	:: " + userId);
         	logger.info("[EnjoyLoginSvc][execute] passWord 	:: " + passWord);
         	
@@ -53,11 +59,42 @@ import th.go.motorcycles.web.enjoy.utils.MotorUtil;
         		easUtil.writeMSG("OK:" + userBean.getCompanyId());
         	}
         	
-        }catch(Exception e){
+        }catch(EnjoyException e){
         	e.printStackTrace();
+        	logger.info(e.getMessage());
+        	easUtil.writeMSG(e.getMessage());
+		}catch(Exception e){
+        	e.printStackTrace();
+        	logger.info(e.getMessage());
         	easUtil.writeMSG(e.getMessage());
         }finally{
         	logger.info("[EnjoyLoginSvc][execute][End]");
         }
 	}
+	
+	private void checkExpiryDate() throws EnjoyException, Exception{
+		Date	  			currDate			= new Date();
+		Date	  			expDate				= null;
+		SimpleDateFormat    dt              	= null;
+		Calendar 			currDateC 			= Calendar.getInstance(Locale.US);
+		
+		try{
+			dt      	= new SimpleDateFormat("yyyyMMdd",Locale.US); 
+			expDate     = dt.parse("20150130"); 
+			
+			currDateC.setTime(currDate);
+			
+			if(currDateC.getTime().after(expDate)){
+				System.out.println("Expiry");
+				throw new EnjoyException("เกิดข้อผิดพลาดในการเข้าสู่ระบบ");
+			}
+            
+		}catch(EnjoyException e){
+			throw e;
+		}catch(Exception e){
+			throw e;
+		}
+	}
+	
+	
 }
